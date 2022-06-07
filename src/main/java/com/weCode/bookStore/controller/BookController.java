97d7ac801cf8d37cfca0659426732520921ea0b8
MercadoJose <jose-eduardo.mercado@atos.net>
2022-06-07 14:13:27 -0500

    Add travis ci file

--- src/main/java/com/weCode/bookStore/controller/BookController.java
+++ src/main/java/com/weCode/bookStore/controller/BookController.java
@@ -1,7 +1,6 @@
 package com.weCode.bookStore.controller;
 
 import com.weCode.bookStore.dto.BookDto;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
